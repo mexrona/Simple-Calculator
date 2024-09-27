c7ee49f066ba5a40b5a134c6fcb28eea02e7a96e
mexrona <tuichievamexrona@gmail.com>
2024-09-27 19:48:03 +0300

    Hi, Simple Calculator!

---
+++ script.js
@@ -0,0 +1,39 @@
+let operator;
+
+const simpleCalculator = () => {
+    let result;
+    const resultInner = document.getElementById("result");
+    const numberOne = Number(document.getElementById("numberOne").value);
+    const numberTwo = Number(document.getElementById("numberTwo").value);
+
+    switch (operator) {
+        case "+":
+            result = numberOne + numberTwo;
+            break;
+        case "-":
+            result = numberOne - numberTwo;
+            break;
+        case "*":
+            result = numberOne * numberTwo;
+            break;
+        case ":":
+            result = numberOne / numberTwo;
+            break;
+    }
+
+    const isUndefined = result === undefined;
+    const condition = isUndefined ? "Ошибка" : result;
+
+    if (condition === "Ошибка") {
+        resultInner.style.color = "red";
+    } else {
+        resultInner.style.color = "green";
+    }
+
+    resultInner.innerHTML = condition;
+
+    setTimeout(() => {
+        numberOne.innerHTML = "";
+        numberTwo.innerHTML = "";
+    }, 3000);
+};
